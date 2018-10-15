c146c058b3a443a44c10dad1b0f4bc3788fed479
alexcengler <alexcengler@gmail.com>
2018-10-15 18:44:19 -0500

    Remove sudo from shell script (it is run as root) and added notes on that script to markdown

--- ec2-rserver.sh
+++ ec2-rserver.sh
@@ -1,19 +1,19 @@
 #!/bin/bash 
 set -x –e
 
-sudo yum update 
-sudo yum install -y R
+yum update 
+yum install -y R
 
 wget https://download2.rstudio.org/rstudio-server-rhel-1.1.456-x86_64.rpm 
 
-sudo yum install rstudio-server-rhel-1.1.456-x86_64.rpm -y
+yum install rstudio-server-rhel-1.1.456-x86_64.rpm -y
 
 rm rstudio-server-rhel-1.1.456-x86_64.rpm
 
-sudo R -e "install.packages(c('shiny','doParallel'), repos='http://cran.rstudio.com/')"
+R -e "install.packages(c('shiny','doParallel'), repos='http://cran.rstudio.com/')"
 
-sudo useradd ec2-user
+useradd ec2-user
 echo ec2-user:RisGreat | sudo chpasswd 
 
-sudo yum install -y git
+yum install -y git
 git clone https://github.com/alexcengler/aws-workshop.git
