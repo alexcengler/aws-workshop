33f5c9c8952dd294e8eec3d7613b70fd8ef5785a
Alex Engler <alexcengler@gmail.com>
2020-04-01 15:01:10 -0400

    Lots of changes

--- ec2-rserver.sh
+++ ec2-rserver.sh
@@ -10,10 +10,10 @@ yum install rstudio-server-rhel-1.1.456-x86_64.rpm -y
 
 rm rstudio-server-rhel-1.1.456-x86_64.rpm
 
-R -e "install.packages(c('shiny','doParallel'), repos='http://cran.rstudio.com/')"
+R -e "install.packages(c('doParallel','foreach'), repos='http://cran.rstudio.com/')"
 
 useradd test
-echo test:test | chpasswd 
+echo test:test_pw | chpasswd 
 
 yum install -y git
 git clone https://github.com/alexcengler/aws-workshop.git /home/ec2-user/aws-workshop
